7d30653d5c7dc5e81c2fa3a6f0b922206a9ccffb
Plofland <ploflan@gmail.com>
2021-02-28 20:02:42 -0500

    DB seeded

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,7 +1,11 @@
 exports.seed = function (knex) {
   return knex('users').insert([
-    { user_id: 1, username: 'Peter' },
-    { user_id: 2, username: 'OscFig' },
-    { user_id: 3, username: 'xXx_3lite_Snipes_xXx' }
+    { user_id: 1, username: 'Peter', password: 'abc123' },
+    { user_id: 2, username: 'OscFig', password: 'abc123' },
+    {
+      user_id: 3,
+      username: 'xXx_3lite_Snipes_xXx',
+      password: 'abc123'
+    }
   ]);
 };
--- data/seeds/02-tech_hardware.js
+++ data/seeds/02-tech_hardware.js
@@ -1,7 +1,24 @@
 exports.seed = function (knex) {
-  return knex('table_name').insert([
-    { tech_id: 1, name: 'Microphone', description: 'Audio-Technica 2035 cardioid condenser XLR microphone' },
-    { tech_id: 2, name: 'DSLR Camera', description: 'Canon 5d mkIII DSLR camera' },
-    { tech_id: 3, name: 'Audio Interface', description: 'Focusrite Scarlett Solo USB audio interface' }
+  return knex('tech_hardware').insert([
+    {
+      tech_id: 1,
+      name: 'Microphone',
+      description:
+        'Audio-Technica 2035 cardioid condenser XLR microphone',
+      user_id: 1
+    },
+    {
+      tech_id: 2,
+      name: 'DSLR Camera',
+      description: 'Canon 5d mkIII DSLR camera',
+      user_id: 2
+    },
+    {
+      tech_id: 3,
+      name: 'Audio Interface',
+      description:
+        'Focusrite Scarlett Solo USB audio interface',
+      user_id: 1
+    }
   ]);
 };
