b0982f39728fc4f536481fd2c0d7f3906e83e672
Plofland <ploflan@gmail.com>
2021-03-03 13:38:10 -0500

    Fixed duplicate id key bug

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -27,13 +27,13 @@ function findBy(filter) {
 }
 
 async function add(user) {
-  const [id] = await db('users').insert(user, 'user_id');
+  const [id] = await db('users').insert(user, 'id');
   return findById(id);
 }
 
 function findById(id) {
   return db('users as u')
-    .select('u.user_id', 'u.username')
+    .select('u.user_id', 'u.username', 'u.email')
     .where('u.user_id', id)
     .first();
 }
