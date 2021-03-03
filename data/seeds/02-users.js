9af6e00c8df17b6063272e77ab3d5e52277a4caf
Plofland <ploflan@gmail.com>
2021-03-02 20:06:39 -0500

    added email column to user table

--- data/seeds/02-users.js
+++ data/seeds/02-users.js
@@ -3,18 +3,21 @@ exports.seed = function (knex) {
     {
       user_id: 1,
       username: 'Peter',
+      email: 'peter@peter.com',
       password: 'abc123',
       role_id: 2
     },
     {
       user_id: 2,
       username: 'OscFig',
+      email: 'oscfig@oscfig.com',
       password: 'abc123',
       role_id: 2
     },
     {
       user_id: 3,
       username: 'xXx_3lite_Snipes_xXx',
+      email: 'snipes@snipes.com',
       password: 'abc123',
       role_id: 1
     }
