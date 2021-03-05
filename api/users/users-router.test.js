12316ae664baf16813cb1628cd56868e2816032c
Plofland <ploflan@gmail.com>
2021-03-04 19:11:16 -0500

    set up server sanity test

--- api/users/users-router.test.js
+++ api/users/users-router.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
-const server = require('./server');
-const db = require('../data/dbConfig');
+const server = require('../server');
+const db = require('../../data/dbConfig');
 
 test('sanity', () => {
   expect(true).toBe(true);
 });
 
-const paul = { username: 'Paul', password: 'abc123' };
+const paul = { username: 'Paul', email: 'paul@paul.com', password: 'abc123' };
 
 //Set-up to handle migrations & destruction of test DB before and after each test
 beforeAll(async () => {
@@ -21,3 +21,10 @@ beforeEach(async () => {
 afterAll(async () => {
   await db.destroy();
 });
+
+describe('users endpoints', () => {
+  describe('server is up and running', async () => {
+    const res = request(server).get('/');
+    expect(res.body.api).toBe('up running')
+  })
+})
